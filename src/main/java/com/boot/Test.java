c2a9e51bc7fcc3093e883c7a7d9edab9e0326b7f
shen <308070769@qq.com>
2022-04-23 22:28:08 +0800

    分支02 init

--- src/main/java/com/boot/Test.java
+++ src/main/java/com/boot/Test.java
@@ -5,7 +5,7 @@ public class Test {
     public static void main(String args[]){
         System.out.println("get_test");
         System.out.println("get_test");
-        System.out.println("get_test");
+        System.out.println("幽幽幽幽幽幽幽幽幽幽幽幽幽幽幽幽幽幽幽幽幽幽幽幽");
         System.out.println("get_test");
     }
 }
