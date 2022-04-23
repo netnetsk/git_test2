b63e27018839289f9bf0542f674abc141b051abf
netnetsk <58058460+netnetsk@users.noreply.github.com>
2022-04-23 22:24:58 +0800

    Update Test.java

--- src/main/java/com/boot/Test.java
+++ src/main/java/com/boot/Test.java
@@ -6,5 +6,6 @@ public class Test {
         System.out.println("get_test");
         System.out.println("get_test");
         System.out.println("get_test");
+        System.out.println("get_test");
     }
 }
