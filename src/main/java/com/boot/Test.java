b59073aeba55818c2bf2adeb817f3c01a4049b4b
308070769@qq.com <netsk2018>
2022-08-12 21:58:01 +0800

    code modify

--- src/main/java/com/boot/Test.java
+++ src/main/java/com/boot/Test.java
@@ -3,10 +3,14 @@ package com.boot;
 public class Test {
 
     public static void main(String args[]){
-        System.out.println("get_test1");
-        System.out.println("get_test2");
-        System.out.println("get_test3");
-        System.out.println("get_test4");
-        System.out.println("get_test5");
+
+    }
+
+    public void abc(){
+        try {
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
